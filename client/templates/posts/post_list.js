bb8411f557df5dde9ab0c3e7c6e5749f1b511156
leon mak <leonmakansheng@gmail.com>
2016-01-22 19:45:20 +0800

    added collections mock

--- client/templates/posts/post_list.js
+++ client/templates/posts/post_list.js
@@ -1,16 +1,16 @@
-var postsData = [ {
-  title: 'Introducing Telescope',
-  url: 'http://sachagreif.com/introducing-telescope/'
-},
-{
-  title: 'Meteor',
-  url: 'http://meteor.com'
-}, {
-  title: 'The Meteor Book',
-  url: 'http://themeteorbook.com'
-}
-];
+// var postsData = [ {
+//   title: 'Introducing Telescope',
+//   url: 'http://sachagreif.com/introducing-telescope/'
+// },
+// {
+//   title: 'Meteor',
+//   url: 'http://meteor.com'
+// }, {
+//   title: 'The Meteor Book',
+//   url: 'http://themeteorbook.com'
+// }
+// ];
 
 Template.postList.helpers({
- posts: postsData
+ posts: function(){ return Posts.find() }
 })
